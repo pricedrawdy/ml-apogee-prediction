ce68e37caed4874578d032378752aa145fd6c8cf
Price Drawdy <124943728+pricedrawdy@users.noreply.github.com>
2025-08-24 14:20:50 -0400

    Load rocket parameters from JSON

--- scripts/batch_simulation_creation.py
+++ scripts/batch_simulation_creation.py
@@ -1,76 +1,90 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
 from rocketpy import Environment, SolidMotor, Rocket, Flight, TrapezoidalFins, EllipticalFins, RailButtons, NoseCone, Tail, Parachute
 import datetime
 import matplotlib.pyplot as plt
 
+from rocket_parameter_loader import load_parameters
+
+
+# Load parameters from JSON
+params = load_parameters()
+data_dir = Path(__file__).resolve().parents[1] / "rocket-info"
+
+motor_params = params["motors"]
+nosecone_params = params["nosecones"]
+fins_params = params["trapezoidal_fins"]["0"]
+tail_params = params["tails"]["0"]
+parachute_params = params["parachutes"]
+rocket_params = params["rocket"]
+rail_button_params = params["rail_buttons"]
+env_params = params["environment"]
+
+thrust_source_path = data_dir / Path(motor_params["thrust_source"]).name
 motor = SolidMotor(
-    thrust_source='thrust_source.csv',
-    dry_mass=0,
-    center_of_dry_mass_position=0,
-    dry_inertia=[0, 0, 0],
-    grains_center_of_mass_position=0,
-    grain_number=1,
-    grain_density=1115.3675185630957,
-    grain_outer_radius=0.049,
-    grain_initial_inner_radius=0.0245,
-    grain_initial_height=0.597,
-    grain_separation=0,
-    nozzle_radius=0.036750000000000005,
-    nozzle_position=-0.2985,
-    throat_radius=0.0245,
+    thrust_source=str(thrust_source_path),
+    dry_mass=motor_params["dry_mass"],
+    center_of_dry_mass_position=motor_params["center_of_dry_mass_position"],
+    dry_inertia=motor_params["dry_inertia"],
+    grains_center_of_mass_position=motor_params["grains_center_of_mass_position"],
+    grain_number=motor_params["grain_number"],
+    grain_density=motor_params["grain_density"],
+    grain_outer_radius=motor_params["grain_outer_radius"],
+    grain_initial_inner_radius=motor_params["grain_initial_inner_radius"],
+    grain_initial_height=motor_params["grain_initial_height"],
+    grain_separation=motor_params["grain_separation"],
+    nozzle_radius=motor_params["nozzle_radius"],
+    nozzle_position=motor_params["nozzle_position"],
+    throat_radius=motor_params["throat_radius"],
     reshape_thrust_curve=False,  # Not implemented in Rocket-Serializer
-    interpolation_method='linear',
-    coordinate_system_orientation='nozzle_to_combustion_chamber',
+    interpolation_method="linear",
+    coordinate_system_orientation=motor_params["coordinate_system_orientation"],
 )
 
 # Nose Cone
 nosecone = NoseCone(
-    length=0.9270999999999999,
-    kind='Von Karman',
-    base_radius=0.078359,
-    rocket_radius=0.078359,
-    name='0.9270999999999999',
+    length=nosecone_params["length"],
+    kind=nosecone_params["kind"],
+    base_radius=nosecone_params["base_radius"],
+    rocket_radius=rocket_params["radius"],
+    name=nosecone_params["name"],
 )
 
 # Fins
 trapezoidal_fins = {}
 trapezoidal_fins[0] = TrapezoidalFins(
-    n=3,
-    root_chord=0.29999939999999997,
-    tip_chord=0.100076,
-    span=0.20246339999999996,
-    cant_angle=0.0,
-    sweep_length= 0.18542,
-    sweep_angle= None,
-    rocket_radius=0.078359,
-    name='Main Fins',
+    n=fins_params["number"],
+    root_chord=fins_params["root_chord"],
+    tip_chord=fins_params["tip_chord"],
+    span=fins_params["span"],
+    cant_angle=fins_params["cant_angle"],
+    sweep_length=fins_params["sweep_length"],
+    sweep_angle=fins_params["sweep_angle"],
+    rocket_radius=rocket_params["radius"],
+    name=fins_params["name"],
 )
 
 # Tails
 tails = {}
 tails[0] = Tail(
-    top_radius=0.078359,
-    bottom_radius=0.05505,
-    length=0.1016,
-    rocket_radius=0.078359,
-    name='Boattail',
+    top_radius=tail_params["top_radius"],
+    bottom_radius=tail_params["bottom_radius"],
+    length=tail_params["length"],
+    rocket_radius=rocket_params["radius"],
+    name=tail_params["name"],
 )
 
 # Parachutes
 parachutes = {}
-parachutes[0] = Parachute(
-    name='Main Bag with Main Chute',
-    cd_s=10.274,
-    trigger=365.760,
-    sampling_rate=100, 
-)
-parachutes[1] = Parachute(
-    name='Drogue Bag with Drogue Parachute',
-    cd_s=0.467,
-    trigger='apogee',
-    sampling_rate=100, 
-)
+for key, p in parachute_params.items():
+    trigger = p["deploy_altitude"] if p["deploy_event"] == "altitude" else p["deploy_event"]
+    parachutes[int(key)] = Parachute(
+        name=p["name"],
+        cd_s=p["cds"],
+        trigger=trigger,
+        sampling_rate=100,
+    )
 
 # Sweep parameters
 wind_speeds = np.arange(0, 12, 3)
@@ -93,37 +107,36 @@ colnames += [f"Altitude (m) @ {t:.2f}s" for t in timesteps]
 all_rows = []
 
 env = Environment()
-env.set_location(latitude=32.9428, longitude=-106.912)
-env.set_elevation(60.96)
+env.set_location(latitude=env_params["latitude"], longitude=env_params["longitude"])
+env.set_elevation(env_params["elevation"])
 tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 env.set_date((tomorrow.year, tomorrow.month, tomorrow.day, 12))
 
-'''wind = 6
-temp = 293
-angle = 4'''
-
 for wind in wind_speeds:
-    env.wind_speed = wind    
+    env.wind_speed = wind
 
     for temp in temps:
         env.temperature = temp
 
         for angle in launch_angles:
             rocket = Rocket(
-                radius=0.078359,
-                mass=21.215,
-                inertia=[0.014, 0.014, 4.458],
-                power_off_drag='drag_curve.csv',
-                power_on_drag='drag_curve.csv',
-                center_of_mass_without_motor=2.122,
-                coordinate_system_orientation='nose_to_tail',
+                radius=rocket_params["radius"],
+                mass=rocket_params["mass"],
+                inertia=rocket_params["inertia"],
+                power_off_drag=str(data_dir / Path(rocket_params["drag_curve"]).name),
+                power_on_drag=str(data_dir / Path(rocket_params["drag_curve"]).name),
+                center_of_mass_without_motor=rocket_params["center_of_mass_without_propellant"],
+                coordinate_system_orientation=rocket_params["coordinate_system_orientation"],
             )
-            rocket.add_surfaces(surfaces=[nosecone, trapezoidal_fins[0], tails[0]], positions=[0.0, 3.0275276000000004, 3.324225])
-            rocket.add_motor(motor, position= 3.1035067693124514)
+            rocket.add_surfaces(
+                surfaces=[nosecone, trapezoidal_fins[0], tails[0]],
+                positions=[nosecone_params["position"], fins_params["position"], tail_params["position"]],
+            )
+            rocket.add_motor(motor, position=motor_params["position"])
             rail_buttons = rocket.set_rail_buttons(
-                upper_button_position=2.578,
-                lower_button_position=4.208,
-                angular_position=0.000,
+                upper_button_position=rail_button_params["upper_position"],
+                lower_button_position=rail_button_params["lower_position"],
+                angular_position=rail_button_params["angular_position"],
             )
             # Flight
             flight = Flight(
@@ -198,17 +211,3 @@ df = pd.DataFrame(all_rows, columns=colnames)
 df.to_csv("batch_dataset_v1.csv", index=False)
 print("Simulation data saved as batch_dataset_v1.csv")
 
-
-
-
-
-
-
-
-'''
-row = [wind, temp, angle, apogee_alt]
-row += v_vel.tolist()
-row += v_acc.tolist()
-row += t_vel.tolist()
-row += alt_interp.tolist()
-'''
